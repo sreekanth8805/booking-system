648ffe0e9ec66a36e8e897f0681f565816f0328b
sreekanth8805 <67037941+sreekanth8805@users.noreply.github.com>
2021-08-18 15:53:54 +0530

    Add files via upload

---
+++ Booking_system/src/booking_system/Booking_system.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package booking_system;
+
+/**
+ *
+ * @author My Pc
+ */
+public class Booking_system {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
